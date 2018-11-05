965581cf5d4557c7ad27b2b0f81517655f1e0b76
acoshift <acoshift@gmail.com>
2018-11-05 22:05:31 +0700

    add global dispatch after, at

--- global.go
+++ global.go
@@ -2,6 +2,7 @@ package dispatcher
 
 import (
 	"context"
+	"time"
 )
 
 // expose global vars
@@ -17,4 +18,14 @@ func Register(hs ...Handler) {
 // Dispatch dispatchs default dispatcher
 func Dispatch(ctx context.Context, msg ...Message) error {
 	return DefaultDispatcher.Dispatch(ctx, msg...)
+}
+
+// DispatchAfter dispatchs default dispatcher
+func DispatchAfter(ctx context.Context, duration time.Duration, resultFn func(err error), msg ...Message) {
+	DefaultDispatcher.DispatchAfter(ctx, duration, resultFn, msg...)
+}
+
+// DispatchAt dispatchs default dispatcher
+func DispatchAt(ctx context.Context, t time.Time, resultFn func(err error), msg ...Message) {
+	DefaultDispatcher.DispatchAt(ctx, t, resultFn, msg...)
 }
